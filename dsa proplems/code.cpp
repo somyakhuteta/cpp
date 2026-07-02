9a730c4b52a4a328d078c215c9e57c0a82bfd607
Somya Khuteta <S25cseu1606@bennett.edu.in>
2026-07-02 09:42:40 +0000

    added code.cpp

---
+++ dsa proplems/code.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <vector>
+
+class Job{
+    int idx;
+    int deadline;
+    int profit;
+    Job(int idx, int deadline, int profit){
+
+    }
+}
+int maxProfit(vector<int>){
+    int n = pairs.size();
+
+}
